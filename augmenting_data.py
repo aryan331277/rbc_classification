3b772605242f3c610c2b7c3ee5cb7455d24588e5
Aryan Karmore <bt24csd009@iiitn.ac.in>
2025-03-22 09:24:33 +0530

    Create augmenting_data.py

---
+++ augmenting_data.py
@@ -0,0 +1,33 @@
+img_height, img_width = 224, 224
+batch_size = 32
+
+train_dir = os.path.join(output_base_dir, "train")
+test_dir = os.path.join(output_base_dir, "test")
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    zoom_range=0.1,
+    horizontal_flip=True
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical',
+    shuffle=True
+)
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical',
+    shuffle=False
+)
+
